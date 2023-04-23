42c435b8941c62c7cba688ef4ef6b4855b2c9ea6
Hernan Cabrera <hcabrerazavala@gmail.com>
2023-04-23 20:25:16 -0300

    adding delete

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Card from "./Card";
 
+// terminar boton de delete
 const ToDoList = ({ list }) => {
 
     return (
         <div>
             {list.map((item, index) => {
                 return (
-                    <Card key={index} item={item} />
+                    <div>
+                        <Card key={index} item={item} /> <button key={index}>x</button>
+                    </div>
                 )
             })}
         </div>
